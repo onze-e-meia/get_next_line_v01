80849c2e26d4ba7165d9b3110735b960ed81ec5a
onze-e-meia <tffuente@gmail.com>
2024-06-25 16:32:20 -0300

    New 1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tforster <tfforster@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 16:53:04 by tforster          #+#    #+#             */
-/*   Updated: 2024/06/25 15:03:12 by tforster         ###   ########.fr       */
+/*   Updated: 2024/06/25 16:31:26 by tforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,6 @@ int	main(int argc, char **argv)
 {
 	char		*gnl;
 	int			fd;
-
-	// int		i = 0;
-	// int		bytes = 0;
-	// char	buffer [7];
-
-	// printf(">>> BYTES [%d]\n STR: [%s]", bytes, buffer);
-	// printf(">>> END\n\n");
-	// while (i <= 12)
-	// {
-	// 	fd = open("test.txt", O_RDONLY);
-	// 	bytes = read(fd, buffer, i);
-	// 	printf("BYTES [%d]\n STR: [%s]", bytes, buffer);
-	// 	printf("END 00\n");
-	// 	int		j = 0;
-	// 	while (j <= 12)
-	// 	{
-	// 		printf("> CHAR %d[%c][%d] ", j, buffer[j], buffer[j]);
-	// 		if (buffer[j] == '\0')
-	// 			printf(" TRUE AT j = [%d]", j);
-	// 		j++;
-	// 		printf("\n");
-	// 	}
-	// 	printf("END 01\n\n");
-
-	// 	close(fd);
-	// 	i++;
-	// }
 
 	if (argc == 1)
 		fd = 1;
